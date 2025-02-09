bf7005ef21f343b401d6f16bf42f6a1f45134fec
Gaëlle Ozon <gozon@student.42.fr>
2025-02-09 11:09:47 +0100

    Add class brain

---
+++ ex01/Brain.cpp
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Brain.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gozon <gozon@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/09 10:51:07 by gozon             #+#    #+#             */
+/*   Updated: 2025/02/09 11:09:14 by gozon            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Brain.hpp"
+
+Brain::Brain() : nbIdeas(0) {
+
+    std::cout << "Brain default constructor called" << std::endl;
+
+}
+
+Brain::Brain(const Brain& copy) : nbIdeas(copy.nbIdeas) {
+
+    for (int i = 0; i < 100; i++) {
+        this->ideas[i] = copy.ideas[i];
+    }
+    std::cout << "Brain copy constructor called" << std::endl;
+
+}
+
+Brain& Brain::operator=(const Brain& copy) {
+
+    this->nbIdeas = copy.nbIdeas;
+    for (int i = 0; i < 100; i++) {
+        this->ideas[i] = copy.ideas[i];
+    }
+    std::cout << "Brain copy assignment operator called" << std::endl;
+
+}
+
+Brain::~Brain() {
+
+    std::cout << "Brain destructor called" << std::endl;
+
+}
+
+void Brain::addIdea(const std::string& idea) {
+
+    if (this->nbIdeas >= 100) {
+        std::cout << "that's too many ideas, brain is full." << std::endl;
+    }
+    else {
+        this->ideas[this->nbIdeas] = idea;
+        this->nbIdeas++;
+        std::cout << "Idea added" << std::endl;
+    }
+
+}
+
+void Brain::printIdeas(void) const {
+
+    for (int i = 0; i < this->nbIdeas; i++) {
+        std::cout << this->ideas[this->nbIdeas] << std::endl;
+    }
+
+}
---
+++ ex01/Brain.hpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Brain.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gozon <gozon@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/09 10:23:14 by gozon             #+#    #+#             */
+/*   Updated: 2025/02/09 10:31:07 by gozon            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BRAIN_HPP
+#define BRAIN_HPP
+
+#include <iostream>
+#include <string>
+
+class Brain {
+
+    private:
+
+        std::string ideas[100];
+        int nbIdeas;
+
+    public:
+
+        Brain();
+        Brain(const Brain& copy);
+        Brain& operator=(const Brain& copy);
+        ~Brain();
+
+        void addIdea(const std::string& idea);
+        void printIdeas(void) const;
+
+
+};
+
+#endif
